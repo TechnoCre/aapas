275dd367d93744257d07ad8902d0a20cacce5476
TechnoCre <rahul89dabral@gmail.com>
2025-05-01 15:22:45 +0530

    update the file of create a/c page and login a/c page.

--- src/pages/CreateAccountPageView.js
+++ src/pages/CreateAccountPageView.js
@@ -54,7 +54,6 @@ const CreateAccountPageView = () => {
             {/* Sign-Up Form */}
             <div className='login-container'>
                 <div className="heading">Sign Up</div>
-<<<<<<< HEAD
                 <form className="form" onSubmit={handleSubmit}>
                     <input
                         required
@@ -92,16 +91,10 @@ const CreateAccountPageView = () => {
                         value={formData.cpassword}
                         onChange={handleChange}
                     />
-=======
-                <form action="" className="form">
-                    <input required className="input" type="email" name="email" id="email" placeholder="E-mail" />
-                    <input required className="input" type="number" name="phone_number" id="phone-number" placeholder="Phone Number" />
-                    {/* <input required className="input" type="date" name="dob" id="dob" placeholder="Date of Birth" /> */}
-                    <input required className="input" type="password" name="password" id="password" placeholder="Password" />
-                    <input required className="input" type="password" name="cpassword" id="cpassword" placeholder="Confirm Password" />
->>>>>>> moksh
                     <input className="login-button" type="submit" value="Sign Up" />
                 </form>
+
+                {/* Display Message */}
                 {message && <p className="message">{message}</p>}
 
                 {/* Social Sign-In */}
--- src/pages/LoginPageView.js
+++ src/pages/LoginPageView.js
@@ -48,7 +48,6 @@ const LoginPageView = () => {
 
             {/* Login Form */}
             <div className='login-container'>
-<<<<<<< HEAD
                 <div className="heading">Sign In</div>
                 <form className="form" onSubmit={handleSubmit}>
                     <input
@@ -71,14 +70,6 @@ const LoginPageView = () => {
                     />
                     <span className="forgot-password"><a href="#">Forgot Password?</a></span>
                     <input className="login-button" type="submit" value="Sign In" />
-=======
-                <div className="heading">Log In</div>
-                <form action="" className="form">
-                    <input required="" className="input" type="email" name="email" id="email" placeholder="E-mail" />
-                    <input required="" className="input" type="password" name="password" id="password" placeholder="Password" />
-                    <span className="forgot-password"><a href="#">Forgot Password ?</a></span>
-                    <input className="login-button" type="submit" value="Log In" />
->>>>>>> moksh
                 </form>
 
                 {/* Display Message */}
